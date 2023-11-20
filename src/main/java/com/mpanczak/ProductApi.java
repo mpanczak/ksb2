d1e89b4a9d7d0615e00687d19642205a3e012424
mpanczak <panczakmateusz@gmail.com>
2023-11-20 18:06:29 +0100

    web methods

--- src/main/java/com/mpanczak/ProductApi.java
+++ src/main/java/com/mpanczak/ProductApi.java
@@ -1,2 +1,30 @@
-package com.mpanczak;public class ProductApi {
+package com.mpanczak;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductApi {
+
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+//    @GetMapping()
+    public String getProduct() {
+        return "Hello GET";
+    }
+
+    @PostMapping("/products")
+    public String addProduct() {
+        return "Hello POST";
+    }
+
+    @PutMapping
+    public String putProduct() {
+        return "Hello PUT";
+    }
+
+    @DeleteMapping
+    public String deleteProduct() {
+        return "Hello DELETE";
+    }
 }
